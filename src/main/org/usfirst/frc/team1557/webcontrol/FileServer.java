48a7e4782f4b60de9336bfed9cdaa3ddf3ad65d7
Nickardson <taylorgratzer@yahoo.com>
2015-02-08 02:19:38 -0500

    Added WebSockets

--- src/main/org/usfirst/frc/team1557/webcontrol/FileServer.java
+++ src/main/org/usfirst/frc/team1557/webcontrol/FileServer.java
@@ -1,32 +1,88 @@
 package org.usfirst.frc.team1557.webcontrol;
 
-import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.*;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class FileServer extends NanoHTTPD {
+
+    WebSocketResponseHandler responseHandler;
+
     public FileServer(String host, int port) {
         super(host, port);
+
+        responseHandler = new WebSocketResponseHandler(webSocketFactory);
     }
 
     @Override
     public Response serve(IHTTPSession session) {
-        String uri = session.getUri();
+        Response ws = responseHandler.serve(session);
 
+        // If ws is null, then request is not Websocket related, otherwise it is a WebSocket object.
+        if (ws == null) {
+            String uri = session.getUri();
 
-        if (uri.equals("/")) {
-            uri += "index.html";
+            if (uri.equals("/")) {
+                uri += "index.html";
+            }
+
+            // Serve resources from inside the http package bundled with the jar.
+            uri = "/http" + uri;
+
+            InputStream stream = getClass().getResourceAsStream(uri);
+
+            if (stream == null) {
+                return new Response(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "Not Found: " + uri);
+            }
+
+            return new Response(Response.Status.OK, MIME_HTML, stream);
         }
 
-        // Serve resources from inside the http package bundled with the jar.
-        uri = "/http" + uri;
+        return ws;
+    }
 
-        InputStream stream = getClass().getResourceAsStream(uri);
+    IWebSocketFactory webSocketFactory = new IWebSocketFactory() {
+        @Override
+        public WebSocket openWebSocket(IHTTPSession handshake) {
+            return new Ws(handshake);
+        }
+    };
 
-        if (stream == null) {
-            return new Response(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "Not Found: " + uri);
+    class Ws extends WebSocket {
+        public Ws(IHTTPSession handshakeRequest) {
+            super(handshakeRequest);
+            System.out.println("WS Connect: " + handshakeRequest.getUri());
         }
 
-        return new Response(Response.Status.OK, MIME_HTML, stream);
+        @Override
+        protected void onPong(WebSocketFrame pongFrame) {
+
+        }
+
+        @Override
+        protected void onMessage(WebSocketFrame messageFrame) {
+            // Ignore ping messages
+            if (messageFrame.getTextPayload().length() == 0) {
+                return;
+            }
+
+            System.out.println("Message : " + messageFrame.getTextPayload());
+            try {
+                send("Response to " + messageFrame.getTextPayload());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void onClose(WebSocketFrame.CloseCode code, String reason, boolean initiatedByRemote) {
+            System.out.println("WS Disconnected: " + code.name() + " : " + code.getValue() + " because " + reason);
+        }
+
+        @Override
+        protected void onException(IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
